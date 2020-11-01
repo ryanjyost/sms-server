0a8c837049ca3994f790bcfba685f3823c635bb4
ryanjyost <ryost217@gmail.com>
2020-11-01 01:00:23 -0500

    trying to fix

--- migrations/sqls/20201031042959-user-auth-up.sql
+++ migrations/sqls/20201031042959-user-auth-up.sql
@@ -6,6 +6,7 @@ ALTER TABLE phone ADD COLUMN firebase_id text;
 
 ALTER TABLE phone ADD COLUMN email text;
 
+-- dont need jyust yet
 --CREATE UNIQUE INDEX firebase_id_unique ON phone (firebase_id);
 
 ALTER TABLE phone RENAME TO users;
